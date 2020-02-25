c4438e7f99129c2f415e3170b4c051be261e767a
Matt Davison <mattd7@vt.edu>
2020-02-25 14:55:37 -0500

    midsem

---
+++ app/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/greeting', methods=['GET'])
+def get_greeting():
+ return jsonify(['Hello world!', 'Hello, world?', 'World, Hello!'])
