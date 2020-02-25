f4f21348e02946072d38f0298baa3d4544001367
Matt Davison <mattd7@vt.edu>
2020-02-25 15:52:51 -0500

    cleaner

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/greeting', methods=['GET'])
+@app.route('/greetings', methods=['GET'])
 def get_greeting():
  return jsonify(['Hello world!', 'Hello, world?', 'World, Hello!'])
-
- app.run(host="0.0.0.0")
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
