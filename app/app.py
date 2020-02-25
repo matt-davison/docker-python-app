a8bb5ad65da0f031a30e61ad3774af8911ca5075
Matt Davison <mattd7@vt.edu>
2020-02-25 16:24:37 -0500

    guccied

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('greetings'))
 @app.route('/greetings', methods=['GET'])
-def get_greeting():
+def greetings():
  return jsonify(['Hello world!', 'Hello, world?', 'World, Hello!'])
  
 if __name__ == "__main__":
