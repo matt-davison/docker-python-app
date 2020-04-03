52beba0c5c366dcae937ebf9738e7b205bfec169
Matt Davison <mattd7@vt.edu>
2020-04-03 17:18:11 -0400

    milestone #3

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, redirect, url_for, request
 import time
 import uuid
+import pymongo
+
+mongoClient = pymongo.MongoClient("mongodb://mongo:27017")
+mongoCollection = mongoClient["cs2304"]["blabber"]
 
 app = Flask(__name__)
 '''
@@ -14,7 +18,7 @@ app = Flask(__name__)
     "message": "string"
     }
 '''
-blabs = []
+#blabs = []
 
 #Add a new Blab
 @app.route('/blabs', methods=['POST'])
@@ -40,26 +44,48 @@ def newBlab():
     "message": "string"
     }
     '''
+    '''
     content = request.json
     blabs.append({"id": uuid.uuid4(), 
               "postTime": int(time.time()),
               "author": content.get("author"),
               "message": content.get("message")})
     return jsonify(blabs[-1]), 201, {'Content-Type': 'application/json'}
+    '''
+    content = request.json
+    newBlab = {"id": str(uuid.uuid4()), 
+              "postTime": int(time.time()),
+              "author": content.get("author"),
+              "message": content.get("message")}
+    mongoCollection.insert_one(newBlab)
+    newBlab.pop("_id")
+    return jsonify(newBlab), 201, {'Content-Type': 'application/json'}
+
 
 #Get blabs since
 @app.route('/blabs', methods=['GET'])
 def getBlabs():
+    '''
     sinceTime = request.args.get("createdSince")
     retBlabs = []
     for blab in blabs:
         if blab.get("postTime") >= int(sinceTime):
             retBlabs.append(blab)
     return jsonify(retBlabs), 200, {'Content-Type': 'application/json'}
+    '''
+    sinceTime = request.args.get("createdSince")
+    retBlabs = []
+    for item in mongoCollection.find():
+        blab = item.copy()
+        if blab.get("postTime") >= int(sinceTime):
+            blab.pop("_id")
+            retBlabs.append(blab)
+    return jsonify(retBlabs), 200, {'Content-Type': 'application/json'}
 
 #Delete a Blab by id
 @app.route('/blabs/<id>', methods=['DELETE'])
 def removeBlab(id):
+    '''
     toDel = str(id)
     for blab in blabs:
         if toDel == str(blab.get("id")):
@@ -67,6 +93,23 @@ def removeBlab(id):
             blabs.remove(blab)
             return "200: Blab deleted successfully", 200
     return "404: Blab not found", 404
+    '''
+    '''
+    toDel = str(id)
+    for item in mongoCollection.find(): #try find_one()
+        blab = item.copy()
+        if toDel == str(blab["id"]):
+            mongoCollection.delete_one({"id": id})
+            return "200: Blab deleted successfully", 200
+    return "404: Blab not found", 404
+    '''
+    result = mongoCollection.delete_one({"id": id})
+    if result.deleted_count > 0:
+        return "200: Blab deleted successfully", 200
+    msg = "404: Blab not found: "+str(id)
+    return msg, 404
+
+
 
 '''
 @app.route('/', methods=['GET'])
